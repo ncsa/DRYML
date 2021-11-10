27efcd4501f5b3fce94725a577ce6141d95eb3f9
Matthew Krafczyk <krafczyk.matthew@gmail.com>
2021-11-10 09:56:27 -0600

    Try to create a test for updating an object's definition

--- tests/test_dry_object.py
+++ tests/test_dry_object.py
@@ -1,13 +1,14 @@
 import pytest
 import dryml
 import io
-import objects
 import os
 
 def test_basic_object_1():
+    import objects as objs
+
     # Define simple class
     temp_buffer = io.BytesIO()
-    obj = objects.SimpleObject(10)
+    obj = objs.SimpleObject(10)
 
     # Test that save to buffer works
     assert obj.save_self(temp_buffer)
@@ -21,7 +22,9 @@ def test_basic_object_1():
     assert obj2.version() == 1
 
 def test_basic_object_2():
-    obj = objects.SimpleObject(10)
+    import objects as objs
+
+    obj = objs.SimpleObject(10)
 
     assert obj.save_self('test')
 
@@ -33,3 +36,32 @@ def test_basic_object_2():
     assert obj2.version() == 1
 
     os.remove('test.dry')
+
+@pytest.mark.skip(reason="Currently, I don't know how to properly test updating an object definition")
+def test_basic_object_def_update_1():
+    def build_and_save_obj_1():
+        import objects as objs
+
+        obj = objs.SimpleObject(10)
+
+        buffer = io.BytesIO()
+
+        assert obj.save_self(buffer)
+
+        return obj, buffer
+
+    obj1, buffer = build_and_save_obj_1()
+
+    def build_obj_2(buffer):
+        import objects2 as objs
+
+        obj2 = dryml.load_object(buffer)
+
+        return obj2
+
+    obj2 = build_obj_2(buffer)
+
+    assert obj1 == obj2
+
+    assert obj1.version() == 1
+    assert obj2.version() == 2
