29bbf669936893b3378106438c24f218561e246d
Matthew Krafczyk <krafczyk.matthew@gmail.com>
2021-11-27 15:53:59 -0600

    Move to more pythonic object loading method

--- src/dryml/dry_config.py
+++ src/dryml/dry_config.py
@@ -10,3 +10,9 @@ class DryConfig(dict):
         else:
             file.write(json.dumps(self))
         return True
+
+    def get_hash_str(self):
+        return json.dumps(self)
+
+    def get_hash(self):
+        return hash(self.get_hash_str())
--- src/dryml/dry_object.py
+++ src/dryml/dry_object.py
@@ -8,62 +8,68 @@ import json
 import io
 import zipfile
 import uuid
+import copy
 from typing import Type, IO, Union
 from dryml.dry_config import DryConfig
 
-# Define a base Dry Object 
-class DryObject(object):
-    def __init__(self, *args, dry_args={}, dry_kwargs={}, dry_id=None, **kwargs):
-        super().__init__(*args, **kwargs)
-        # Use DryConfig object to coerse args/kwargs to proper json serializable form.
-        self.dry_kwargs = DryConfig(dry_kwargs)
-        self.dry_args = DryConfig(dry_args)
-        # Generate unique id for this object. (Meant to separate between multiple instances of same object)
-        if dry_id is None:
-            self.dry_kwargs['dry_id'] = str(uuid.uuid4())
-        else:
-            self.dry_kwargs['dry_id'] = dry_id
+def file_resolve(file: str, exact_path:bool = False) -> str:
+    if os.path.splitext(file)[1] == '' and not exact_path:
+        file = f"{file}.dry"
+    return file
 
-    def load_object_imp(self, file: IO[bytes]):
-        # Helper function to load object specific data
-        return True
+def load_zipfile(file: Union[str, IO[bytes]], exact_path:bool=False) -> zipfile.ZipFile:
+    if type(file) is str:
+        file = file_resolve(file, exact_path=exact_path)
+        if not os.path.exists(file):
+            raise ValueError(f"File {filepath} doesn't exist!")
+        file = zipfile.ZipFile(file, mode='r')
+    if type(file) is not zipfile.ZipFile:
+        file = zipfile.ZipFile(file, mode='r')
+    return file
 
-    def save_self(self, file: Union[str, IO[bytes]], version: int=1, **kwargs) -> bool:
-        return DryObject.save_object(self, file, version=version, **kwargs)
-
-class DryObjectFactory(object):
-    def __init__(self, cls, *args, callbacks=[], **kwargs):
-        self.cls = cls
-        self.args = args
-        self.kwargs = kwargs
-        self.callbacks = callbacks
-
-    def add_callback(callback):
-        self.callbacks.append(callback)
-
-    def __call__(self):
-        obj = self.cls(*self.args, **self.kwargs) 
-        for callback in self.callbacks:
-            # Call each callback
-            callback(obj)
-        return obj
-
-def load_object_v1(file: IO[bytes], update:bool=False) -> Type[DryObject]:
-    with file.open('config.json', 'r') as config_file:
+def load_config_v1(dry_file: DryObjectFile):
+    "Helper function for loading a version 1 config file"
+    with dry_file.file.open('config.json', 'r') as config_file:
         config_data = json.loads(config_file.read())
+    return config_data
+
+def load_class_def_v1(dry_file: DryObjectFile, update:bool=True):
+    "Helper function for loading a version 1 class definition"
+    # Get class definition
+    with dry_file.file.open('cls_def.dill') as cls_def_file:
+        if update:
+            cls_def = dill.loads(cls_def_file.read(), ignore=False)
+        else:
+            cls_def = dill.loads(cls_def_file.read())
+    return cls_def
+
+def load_meta_data(dry_file: DryObjectFile):
+    with dry_file.file.open('meta_data.json', 'r') as meta_file:
+        meta_data = json.loads(meta_file.read())
+    return meta_data
+
+class DryObjectFile(object):
+    def __init__(self, file: Union[str, IO[bytes]], exact_path:bool=False):
+        self.file = load_zipfile(file, exact_path=exact_path)
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.file.close()
+
+    def close(self):
+        self.file.close()
+
+def load_object_v1(file: DryObjectFile, update:bool=True) -> Type[DryObject]:
+    config_data = load_config_v1(file)
 
     # Get arguments
     kwargs = config_data['kwargs']
     args = config_data['args']
 
     # Get class definition
-    with file.open('cls_def.dill') as cls_def_file:
-        if update:
-            cls_def = dill.loads(cls_def_file.read(), ignore=False)
-        else:
-            cls_def = dill.loads(cls_def_file.read())
-
-    print(kwargs)
+    cls_def = load_class_def_v1(file, update=update)
 
     # Create object
     obj = cls_def(*args, **kwargs)
@@ -103,44 +109,74 @@ def save_object_v1(obj: Type[DryObject], file: IO[bytes]) -> bool:
 
     return True
 
-def load_zipfile(file: Union[str, IO[bytes]], exact_path:bool=False) -> zipfile.ZipFile:
-    if type(file) is str:
-        if os.path.splitext(file)[1] == '' and not exact_path:
-            file = f"{file}.dry"
-        if not os.path.exists(file):
-            raise ValueError(f"File {filepath} doesn't exist!")
-        file = zipfile.ZipFile(file, mode='r')
-    if type(file) is not zipfile.ZipFile:
-        file = zipfile.ZipFile(file, mode='r')
-    return file
-
-def dump_config(file: Union[str, IO[bytes]], exact_path:bool=False):
-    file = load_zipfile(file)
-    with file.open('config.json', 'r') as config_file:
-        config_data = json.loads(config_file.read())
-    file.close()
-    return config_data
-
 def load_object(file: Union[str, IO[bytes]], update:bool=False, exact_path:bool=False) -> Type[DryObject]:
-    file = load_zipfile(file)
-    with file.open('meta_data.json', 'r') as meta_file:
-        meta_data = json.loads(meta_file.read())
-    version = meta_data['version']
-    if version == 1:
-        result_object = load_object_v1(file, update=update)
-    else:
-        raise RuntimeError(f"DRY version {version} unknown")
-    file.close()
+    with DryObjectFile(file) as file:
+        meta_data = load_meta_data(file)
+        version = meta_data['version']
+        if version == 1:
+            result_object = load_object_v1(file, update=update)
+        else:
+            raise RuntimeError(f"DRY version {version} unknown")
     return result_object
 
 def save_object(obj: Type[DryObject], file: Union[str, IO[bytes]], version: int=1, exact_path:bool=False) -> bool:
     # Handle creation of bytes-like
     if type(file) is str:
-        if os.path.splitext(file)[1] == '' and not exact_path:
-            # No extension specified, add default.
-            file = f"{file}.dry"
+        file = file_resolve(file)
         file = open(file, 'wb')
     if version == 1:
         return save_object_v1(obj, file)
     else:
         raise ValueError(f"File version {version} unknown. Can't save!")
+
+# Define a base Dry Object 
+class DryObject(object):
+    def __init__(self, *args, dry_args={}, dry_kwargs={}, dry_id=None, **kwargs):
+        super().__init__(*args, **kwargs)
+        # Use DryConfig object to coerse args/kwargs to proper json serializable form.
+        self.dry_kwargs = DryConfig(dry_kwargs)
+        self.dry_args = DryConfig(dry_args)
+        # Generate unique id for this object. (Meant to separate between multiple instances of same object)
+        if dry_id is None:
+            self.dry_kwargs['dry_id'] = str(uuid.uuid4())
+        else:
+            self.dry_kwargs['dry_id'] = dry_id
+
+    def load_object_imp(self, file: IO[bytes]) -> bool:
+        # Helper function to load object specific data should return a boolean indicating if loading was successful
+        return True
+
+    def save_self(self, file: Union[str, IO[bytes]], version: int=1, **kwargs) -> bool:
+        return save_object(self, file, version=version, **kwargs)
+
+    def get_hash_str(self, no_id=True):
+        class_hash_str = str(type(self))
+        args_hash_str = self.dry_args.get_hash_str()
+        # Remove dry_id so we can test for object 'class'
+        if no_id:
+            kwargs_copy = self.dry_kwargs.copy()
+            kwargs_copy.pop('dry_id')
+            kwargs_hash_str = kwargs_copy.get_hash_str()
+        else:
+            kwargs_hash_str = self.dry_kwargs.get_hash_str()
+        return class_hash_str+args_hash_str+kwargs_hash_str
+
+    def get_hash(self):
+        return hash(self.get_hash_str())
+
+class DryObjectFactory(object):
+    def __init__(self, cls, *args, callbacks=[], **kwargs):
+        self.cls = cls
+        self.args = args
+        self.kwargs = kwargs
+        self.callbacks = callbacks
+
+    def add_callback(callback):
+        self.callbacks.append(callback)
+
+    def __call__(self):
+        obj = self.cls(*self.args, **self.kwargs) 
+        for callback in self.callbacks:
+            # Call each callback
+            callback(obj)
+        return obj
