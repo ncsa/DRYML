08aab5d1d5d07b45cb0391b3847196a532e4ff8f
Matthew Krafczyk <krafczyk.matthew@gmail.com>
2021-11-29 22:23:35 -0600

    First attempt at DrySelector

--- src/dryml/__init__.py
+++ src/dryml/__init__.py
@@ -1,5 +1,6 @@
-from dryml.dry_object import DryObject, DryObjectFactory, load_object, save_object
 from dryml.dry_config import DryList, DryConfig
+from dryml.dry_object import DryObject, DryObjectFactory, load_object, save_object
+from dryml.dry_selector import DrySelector
 from dryml.dry_model import DryModel
 from dryml.workshop import Workshop
 
--- src/dryml/dry_selector.py
+++ src/dryml/dry_selector.py
@@ -1,9 +1,66 @@
 from dryml.dry_object import DryObject
 
 class DrySelector(object):
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs)
+    "Utility object for selecting for specific dry objects"
+    def __init__(self, cls=None, args=None, kwargs=None):
+         self.cls = cls
+         self.args = args
+         self.kwargs = kwargs
 
-    def __call__(obj: DryObject):
-        # Check whether an object matches this selector
+    @staticmethod
+    def match_objects(key_object, value_object):
+        if callable(key_object):
+            print("Callable branch")
+            return key_object(value_object)
+        if issubclass(type(key_object), list):
+            print("list branch")
+            if not issubclass(type(value_object), list):
+                return False
+            # For now, lists must have the same number of elements
+            if len(key_object) != len(value_object):
+                return False
+            for i in range(len(key_object)):
+                if not DrySelector.match_objects(key_object[i], value_object[i]):
+                    return False
+        if issubclass(type(key_object), dict):
+            print("dict branch")
+            if not issubclass(type(value_object), dict):
+                print(f"value type: {type(value_object)}")
+                return False
+            for key in key_object:
+                print(f"checking key {key}")
+                if key not in value_object:
+                    print(f"key doesn't exist")
+                    return False
+                if not DrySelector.match_objects(key_object[key], value_object[key]):
+                    print(f"values didn't match")
+                    return False
+        print("Value path")
+        if key_object != value_object:
+            return False
+        else:
+            return True
+
+    def __call__(self, obj: DryObject):
+        # If required, check object class
+        if self.cls is not None:
+            print("Checking class")
+            if not DrySelector.match_objects(self.cls, type(obj)):
+                return False
+            print("class matches")
+
+        # Check object args
+        if self.args is not None:
+            print("checking args")
+            if not DrySelector.match_objects(self.args, obj.dry_args):
+                return False
+            print("args match")
+
+        # Check object kwargs
+        if self.kwargs is not None:
+            print("checking kwargs")
+            if not DrySelector.match_objects(self.kwargs, obj.dry_kwargs):
+                return False
+            print("kwargs match")
+
         return True
