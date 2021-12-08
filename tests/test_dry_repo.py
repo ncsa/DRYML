c6090cbd2aa6c4918e5006b2d6382997b73884a9
Matthew Krafczyk <krafczyk.matthew@gmail.com>
2021-12-07 21:25:13 -0600

    Add another DryRepo Test

--- tests/test_dry_repo.py
+++ tests/test_dry_repo.py
@@ -5,7 +5,7 @@ import os
 
 import objects
 
-def test_add_object():
+def test_add_object_1():
     obj = objects.HelloStr(msg='test')
 
     repo = dryml.DryRepo('test_dir')
@@ -19,3 +19,21 @@ def test_add_object():
     assert len(objs) == 1
 
     assert objs[0].get_individual_hash() == obj.get_individual_hash()
+
+def test_add_objects_2():
+    repo = dryml.DryRepo()
+
+    objs = []
+    objs.append(objects.HelloStr(msg='test'))
+    objs.append(objects.HelloInt(msg=10))
+    objs.append(objects.HelloInt(msg=10))
+    objs.append(objects.TestClassA(item=[10,10]))
+    objs.append(objects.TestClassB('test'))
+
+    for obj in objs:
+        repo.add_object(obj)
+
+    assert len(repo.get_objs(selector=dryml.DrySelector(cls=objects.HelloStr, kwargs={'msg': 'test'}))) == 1
+    assert len(repo.get_objs(selector=dryml.DrySelector(cls=objects.HelloInt, kwargs={'msg': 10}))) == 2
+    assert len(repo.get_objs(selector=dryml.DrySelector(cls=objects.TestClassA, kwargs={'item': [10,10]}))) == 1
+    assert len(repo.get_objs(selector=dryml.DrySelector(cls=objects.TestClassB, args=['test']))) == 1
