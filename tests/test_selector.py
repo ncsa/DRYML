9c3b6fe721325c652e5cf6b7eb5737019dfac30e
Matthew Krafczyk <krafczyk.matthew@gmail.com>
2021-12-07 21:55:54 -0600

    Add selector test for subclasses

--- tests/test_selector.py
+++ tests/test_selector.py
@@ -53,3 +53,36 @@ def test_selector_3():
 
     assert not sel(obj1)
     assert sel(obj2)
+
+def test_selector_3():
+    "superclass selection"
+    obj1 = objects.TestClassA(base_msg="Test1", item='a')
+    obj2 = objects.TestClassA(base_msg="Test2", item=[10,10,10])
+    obj3 = objects.TestClassB(0, base_msg="Test3")
+    obj4 = objects.TestClassB([10, 10], base_msg="Test4")
+    obj5 = objects.HelloInt(msg=5)
+    obj6 = objects.HelloInt(msg=20)
+    obj7 = objects.HelloStr(msg='test')
+    obj8 = objects.HelloStr(msg='2test')
+
+    sel = dryml.DrySelector(cls=objects.TestBase)
+
+    assert sel(obj1)
+    assert sel(obj2)
+    assert sel(obj3)
+    assert sel(obj4)
+    assert not sel(obj5)
+    assert not sel(obj6)
+    assert not sel(obj7)
+    assert not sel(obj8)
+
+    sel = dryml.DrySelector(cls=objects.HelloObject)
+
+    assert not sel(obj1)
+    assert not sel(obj2)
+    assert not sel(obj3)
+    assert not sel(obj4)
+    assert sel(obj5)
+    assert sel(obj6)
+    assert sel(obj7)
+    assert sel(obj8)
