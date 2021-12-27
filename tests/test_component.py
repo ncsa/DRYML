8e543d8a48bf3da3bf45c470c3ab2325422659ca
Matthew Krafczyk <krafczyk.matthew@gmail.com>
2021-12-27 14:33:21 -0600

    Remove unnecessary fixture def

--- tests/test_component.py
+++ tests/test_component.py
@@ -4,15 +4,7 @@ import os
 import uuid
 
 
-@pytest.fixture
-def create_name():
-    tempfile = str(uuid.uuid4())
-    yield tempfile
-    fullpath = f"{tempfile}.dry"
-    if os.path.exists(fullpath):
-        os.remove(fullpath)
-
-
+@pytest.mark.usefixtures("create_name")
 def test_basic_component_1(create_name):
     import objects
 
