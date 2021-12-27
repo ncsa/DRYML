a0ad30ddecc9237aab7417887938e3f876a44852
Matthew Krafczyk <krafczyk.matthew@gmail.com>
2021-12-27 14:33:50 -0600

    Remove old print statements

--- tests/test_dry_collections.py
+++ tests/test_dry_collections.py
@@ -1,6 +1,7 @@
 from dryml import DryList
 from dryml import DryRepo
 import objects
+import fixtures
 
 
 def test_dry_list_1():
@@ -8,10 +9,12 @@ def test_dry_list_1():
     obj2 = objects.HelloStr(msg="a test")
     the_list = DryList(obj1, obj2)
 
-    print(the_list.definition())
     new_list = the_list.definition().build()
     assert new_list[0].definition() == obj1.definition()
     assert new_list[1].definition() == obj2.definition()
+    assert new_list is not the_list
+    assert new_list[0] is not obj1
+    assert new_list[1] is not obj2
 
 
 def test_dry_list_2():
@@ -45,25 +48,20 @@ def test_dry_list_3():
     # Create a repo
     repo = DryRepo()
 
-    print("Creating objects")
     # Create objects
     obj1 = objects.HelloInt(msg=5)
     obj2 = objects.HelloStr(msg="a test")
 
-    print("Add objects to repo")
     # Add them to the repo
     repo.add_object(obj1)
     repo.add_object(obj2)
 
-    print("create list object")
     # Create a list object
     the_list = DryList(obj1, obj2)
 
-    print("add list object to repo")
     # Add that to the repo
     repo.add_object(the_list)
 
-    print("Build new list object")
     # Create a new list object from a definition, but use a repo
     new_list = the_list.definition().build(repo=repo)
 
