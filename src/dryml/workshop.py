e26effcbb80a675b0494e1ed67c6995461ee47d9
Matthew Krafczyk <krafczyk.matthew@gmail.com>
2022-01-14 15:13:05 -0600

    Add some changes to workshop

--- src/dryml/workshop.py
+++ src/dryml/workshop.py
@@ -5,20 +5,23 @@ from dryml.dry_component import DryComponent
 class Workshop(object):
     def __init__(self, *args, work_dir=None, create_work_dir=True, **kwargs):
         super().__init__(**kwargs)
-        self.repo = DryRepo(work_dir, create=create_work_dir)
+        self.repo = DryRepo(directory=work_dir, create=create_work_dir)
 
     def data_prep(self):
         raise RuntimeError("Not implemented for base workshop")
 
     def train_component(self, obj: DryComponent, *args, **kwargs):
+        obj.train(self.train_data, *args, **kwargs)
+
+    def train_component_outer(self, obj: DryComponent, *args, **kwargs):
         if obj.train_state == DryComponent.untrained:
-            obj.train(*args, **kwargs)
+            self.train_component(obj, *args, **kwargs)
 
     def train_models(self, *args,
                      selector=None,
                      sel_args=None, sel_kwargs=None,
                      **kwargs):
-        self.repo.apply(self.train_component,
+        self.repo.apply(self.train_component_outer,
                         func_args=args,
                         func_kwargs=kwargs,
                         selector=selector,
