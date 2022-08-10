276c767f8b0c278acfd2c2e258335ab9a1b52dda
Matthew Krafczyk <krafczyk.matthew@gmail.com>
2022-08-10 10:46:24 -0500

    Added new tensorflow workflow test.

--- tests/test_end_to_end.py
+++ tests/test_end_to_end.py
@@ -212,3 +212,82 @@ def test_train_supervised_model_tf_1():
     test_loss = test_method(test_data, model)
 
     assert train_loss == test_loss
+
+
+def test_train_supervised_model_tf_2():
+    # Generate classes
+    num_classes = 10
+    num_dims = 2
+    L = -10.
+    H = 10.
+    Max_W = 5.
+    centers = (np.random.random((num_classes, num_dims))*(H-L))+L
+    widths = np.random.random((num_classes, num_dims))*Max_W
+
+    num_train = 5000
+    train_data = mc.gen_dataset_2(
+        num_examples=num_train,
+        centers=centers,
+        widths=widths)
+
+    num_test = 100
+    test_data = mc.gen_dataset_2(
+        num_examples=num_test,
+        centers=centers,
+        widths=widths)
+
+    import dryml.models.tf
+    import dryml.data.transforms
+    import dryml.data.tf.transforms
+    import tensorflow as tf
+
+    model = dryml.models.tf.TFKerasTrainable(
+        model=dryml.models.tf.KerasSequentialFunctionalModel(
+            input_shape=(num_dims,),
+            layer_defs=[
+                ('Dense', {'units': 32, 'activation': 'relu'}),
+                ('Dense', {'units': 32, 'activation': 'relu'}),
+                ('Dense', {'units': num_classes, 'activation': 'softmax'}),
+            ]),
+        optimizer=dryml.models.tf.TFObjectWrapper(tf.keras.optimizers.Adam),
+        loss=dryml.models.tf.TFObjectWrapper(
+            tf.keras.losses.SparseCategoricalCrossentropy),
+        train_fn=dryml.models.tf.TFBasicTraining(epochs=1))
+
+    best_cat = dryml.data.tf.transforms.BestCat()
+
+    pipe = dryml.models.DryPipe(model, best_cat)
+
+    def test_model(test_data, model):
+        import dryml.metrics
+        test_ds = dryml.data.NumpyDataset(test_data, supervised=True)
+
+        return dryml.metrics.categorical_accuracy(model, test_ds)
+
+    @dryml.compute_context(
+        ctx_use_existing_context=False,
+        ctx_update_objs=True)
+    def train_method(train_data, test_data, model):
+        # Create datasets
+        train_ds = dryml.data.NumpyDataset(
+            train_data,
+            batch_size=num_train,
+            supervised=True)
+
+        # Train model
+        model.prep_train()
+        model.train(train_ds)
+
+        return test_model(test_data, model)
+
+    @dryml.compute_context(ctx_use_existing_context=False)
+    def test_method(test_data, model):
+        return test_model(test_data, model)
+
+    random_guessing_accuracy = 1./num_classes
+
+    train_accuracy = train_method(train_data, test_data, pipe)
+    test_accuracy = test_method(test_data, pipe)
+
+    assert train_accuracy == test_accuracy
+    assert train_accuracy > random_guessing_accuracy
