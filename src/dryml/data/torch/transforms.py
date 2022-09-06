c95808f01c204801c5526d7a0559e5c710e00e4e
Matthew Krafczyk <krafczyk.matthew@gmail.com>
2022-09-06 16:25:26 -0500

    Add TorchActivation Trainable
    In case I can't find a way to selectively apply an activation function
    at the end of a model.

--- src/dryml/data/torch/transforms.py
+++ src/dryml/data/torch/transforms.py
@@ -1,3 +1,4 @@
+from dryml.dry_config import DryMeta
 from dryml.models import DryTrainable
 from dryml.data import DryData
 from dryml.data.torch import TorchDataset
@@ -17,3 +18,28 @@ class TorchDevice(DryTrainable):
 
         return data.map_el(
             lambda el: el.to(self.device))
+
+
+class TorchActivation(DryTrainable):
+    @DryMeta.collect_args
+    @DryMeta.collect_kwargs
+    def __init__(self, cls, *args, **kwargs):
+        self.cls = cls
+        self.args = args
+        self.kwargs = kwargs
+        self.act = None
+
+        self.train_state = DryTrainable.trained
+
+    def train(self, *args, train_spec=None, **kwargs):
+        pass
+
+    def compute_prepare_imp(self):
+        self.act = self.cls(*self.args, **self.kwargs)
+
+    def compute_cleanup_imp(self):
+        del self.act
+        self.act = None
+
+    def eval(self, data: DryData, *args, **kwargs):
+        return data.apply_X(lambda X: self.act.forward(X))
