8256f49c05a2188fc455f9695e7166199925477a
Matthew Krafczyk <krafczyk.matthew@gmail.com>
2022-10-10 11:41:50 -0500

    Set version 0.1.0, update dependencies

--- src/dryml/__init__.py
+++ src/dryml/__init__.py
@@ -13,7 +13,7 @@ from dryml.workshop import Workshop
 from dryml.context import compute_context, compute
 import dryml.context as context
 
-__version__ = "0.0.0"
+__version__ = "0.1.0"
 
 __all__ = [
     Object,
