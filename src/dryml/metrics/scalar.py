260d71d07f678c50d05b861dad73e29f51f18c26
tejalkul <102573760+tejalkul@users.noreply.github.com>
2023-07-25 22:57:22 +0530

    Update scalar.py
    Added f1_score functionality. It may not be entirely general. Code is specific to my case.

--- src/dryml/metrics/scalar.py
+++ src/dryml/metrics/scalar.py
@@ -47,3 +47,53 @@ def categorical_accuracy(trainable: Trainable, test_data: Dataset):
         num_total += len(Y)
 
     return num_correct/num_total
+
+
+#Added F1 score functionality.
+@compute_context(ctx_dont_create_context=True)
+def binary_f1_score(trainable: Trainable, test_data: Dataset):
+    if not test_data.supervised:
+        raise ValueError("Dataset is unsupervised!")
+        
+    if not test_data.batched:
+        data = test_data.batch()
+
+    # Prepare the input data, expects a dataset
+    data = trainable.eval(data) \
+                    .as_not_indexed()
+    
+    true_neg = 0
+    true_pos = 0
+    false_neg = 0
+    false_pos = 0
+    
+    for y_pred, y in data.numpy():
+        for i in range(y.shape[0]):
+            for j in range(y.shape[1]):
+                if(y[i][j]==0):
+                    if(y_pred[i][j]==0):
+                        true_neg += 1
+                    else:
+                        false_pos += 1
+                else:
+                    if(y_pred[i][j]==0):
+                        false_neg += 1
+                    else:
+                        true_pos += 1
+    
+    if(true_pos==0 and false_pos==0):
+        recall = 0
+    else:
+        recall = true_pos/(true_pos + false_pos)
+        
+    if(true_pos==0 and false_neg==0):
+        precision = 0
+    else:
+        precision = true_pos/(true_pos + false_neg)
+    
+    if(precision==0 and recall==0):
+        f1_score = 0
+    else:
+        f1_score = 2*precision*recall/(precision + recall)
+    
+    return f1_score
