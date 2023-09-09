f50bc4d9f1b23f16c7daf534a7b5bd21a6899e1f
Matthew Krafczyk <krafczyk.matthew@gmail.com>
2023-09-09 07:34:02 -0500

    Better implementation of core2 components

--- src/dryml/core2.py
+++ src/dryml/core2.py
@@ -1,34 +1,78 @@
-class Meta(type):
-    # Since methods are part of the class, we only have to remove data from the object. We mark the protected data here. Keep up to date with attributes added 
-    __orig_attrs__ = [
-        '__args__',
-        '__kwargs__',
-        '__initialized__',
-        '__locked__',
-    ]
+import uuid
+import time
 
-    @staticmethod
-    def check_existing_attributes(obj):
-        # Check if these attributes are already defined. Throw an error if they are.
-        colliding_attrs = []
-        for attr in Meta.__orig_attrs__:
-            if hasattr(obj, attr):
-                colliding_attrs.append(attr)
-        if len(colliding_attrs) > 0:
-            raise AttributeError(f"Attributes {colliding_attrs} already exist on object. Cannot create object.")
+
+def collide_attributes(obj, attr_list):
+    # Check if these attributes are already defined. Throw an error if they are.
+    colliding_attrs = []
+    for attr in attr_list:
+        if hasattr(obj, attr):
+            colliding_attrs.append(attr)
+    if len(colliding_attrs) > 0:
+        raise AttributeError(f"Attributes {colliding_attrs} already exist on object. Cannot create object.")
+
+
+class CreationControl(type):
+    # Support metaclass to allow more complex metaclass behavior
+    def __create_instance__(cls):
+        return cls.__new__(cls)
 
     def __call__(cls, *args, **kwargs):
-        ## Initial creation
-        obj = cls.__new__(cls)
-        Meta.check_existing_attributes(obj)
-        obj.__args__ = args
-        obj.__kwargs__ = kwargs
-        obj.__locked__ = False
-        obj.__initialized__ = False
+        obj = cls.__create_instance__()
+        args, kwargs = obj.__arg_manipulation__(*args, **kwargs)
+        obj.__pre_init__(*args, **kwargs)
+        obj.__initialize_instance__(*args, **kwargs)
         return obj
 
 
-class Object(metaclass=Meta):
+class Object(metaclass=CreationControl):
+    def __arg_manipulation__(self, *args, **kwargs):
+        return args, kwargs
+
+    def __pre_init__(self, *args, **kwargs):
+        pass
+
+    def __initialize_instance__(self, *args, **kwargs):
+        return self.__init__(*args, **kwargs)
+
+    def __init__(self):
+        pass
+
+
+class Remember(Object):
+    # Support class which remembers the arguments used when creating it.
+    def __pre_init__(self, *args, **kwargs):
+        super().__pre_init__(*args, **kwargs)
+        collide_attributes(self, [
+            '__args__',
+            '__kwargs__',])
+        self.__args__ = args
+        self.__kwargs__ = kwargs
+
+    @property
+    def definition(self):
+        return {
+            'cls': type(self),
+            'args': self.__args__,
+            'kwargs': self.__kwargs__, }
+
+
+class Defer(Remember):
+    # Since methods are part of the class, we only have to remove data from the object. We mark the protected data here. Keep up to date with attributes added 
+    def __pre_init__(self, *args, **kwargs):
+        super().__pre_init__(*args, **kwargs)
+        collide_attributes(self, [
+            '__initialized__',
+            '__locked__',])
+        self.__initialized__ = False
+        self.__locked__ = False
+        self.__orig_keys__ = None
+        self.__orig_keys__ = list(self.__dict__.keys())
+
+    def __initialize_instance__(self, *args, **kwargs):
+        # We explicitly don't initialize the instance.
+        pass
+
     def __getattribute__(self, name):
         # First, check if we have this attribute
         try:
@@ -41,19 +85,46 @@ class Object(metaclass=Meta):
         return super().__getattribute__(name)
     
     def __initialize__(self):
+        if self.__locked__:
+            raise RuntimeError("Cannot initialize object. Object is locked.")
         self.__init__(*self.__args__, **self.__kwargs__)
         self.__initialized__ = True
 
-    def _unload(self):
+    def __unload__(self):
+        if self.__locked__:
+            raise RuntimeError("Cannot unload object. Object is locked.")
         # Remove all attributes besides self._orig_attrs
         for attr in list(self.__dict__.keys()):
-            if attr not in Meta.__orig_attrs__:
+            if attr not in self.__orig_keys__:
                 delattr(self, attr)
         self.__initialized__ = False
 
-    @property
-    def definition(self):
-        return {
-            'cls': type(self),
-            'args': self.__args__,
-            'kwargs': self.__kwargs__, }
+
+class UniqueID(Object):
+    def __arg_manipulation__(self, *args, **kwargs):
+        args, kwargs = super().__arg_manipulation__(*args, **kwargs)
+        if 'uid' not in kwargs:
+            kwargs['uid'] = str(uuid.uuid4())
+        return args, kwargs
+
+    def __init__(self, *args, uid=None, **kwargs):
+        super().__init__(*args, **kwargs)
+        # unique ID
+        self.uid = uid
+
+
+class Metadata(Object):
+    def __arg_manipulation__(self, *args, **kwargs):
+        args, kwargs = super().__arg_manipulation__(*args, **kwargs)
+        if 'metadata' not in kwargs:
+            kwargs['metadata'] = {
+            }
+        if 'description' not in kwargs['metadata']:
+            kwargs['metadata']['description'] = ""
+        if 'creation_time' not in kwargs['metadata']:
+            kwargs['metadata']['creation_time'] = time.time()
+        return args, kwargs
+
+    def __init__(self, *args, metadata=None, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.metadata = metadata
