def6694f86ebf4d94d5f1ace52f3e71d68a91d47
Matthew Krafczyk <krafczyk.matthew@gmail.com>
2023-09-11 11:57:26 -0500

    Add two tests related to numpy arguments

--- tests/test_core2_1.py
+++ tests/test_core2_1.py
@@ -1,4 +1,5 @@
 import pytest
+import numpy as np
 import core2_objects as objects 
 from dryml.core2 import Definition, build_definition
 
@@ -48,4 +49,35 @@ def test_build_definition_2():
     assert len(sub_def.args) == 1
     assert sub_def.args[0] == 10
     assert len(sub_def.kwargs.keys()) == 1
+    assert sub_def.kwargs['test'] == 'b'
+
+
+def test_build_definition_3():
+    # with numpy array argument. algorithm should avoid it
+    arr = np.random.random((2,2)).astype(np.float32)
+    obj = objects.TestClass1(arr, test='a')
+    definition = build_definition(obj)
+    assert definition.cls == objects.TestClass1
+    assert len(definition.args) == 1
+    assert len(definition.kwargs.keys()) == 1
+    assert np.all(definition.args[0] == arr)
+    assert definition.kwargs['test'] == 'a'
+
+
+def test_build_definition_4():
+    # with numpy array argument. algorithm should avoid it
+    # This time nested
+    arr1 = np.random.random((2,2)).astype(np.float32)
+    arr2 = np.random.random((2,2)).astype(np.float32)
+    obj = objects.TestClass1(objects.TestClass1(arr2, test='b'), test=arr1)
+    definition = build_definition(obj)
+    assert definition.cls == objects.TestClass1
+    assert len(definition.args) == 1
+    assert len(definition.kwargs.keys()) == 1
+    assert np.all(definition.kwargs['test'] == arr1)
+    sub_def = definition.args[0]
+    assert sub_def.cls == objects.TestClass1
+    assert len(sub_def.args) == 1
+    assert len(sub_def.kwargs.keys()) == 1
+    assert np.all(sub_def.args[0] == arr2)
     assert sub_def.kwargs['test'] == 'b'
