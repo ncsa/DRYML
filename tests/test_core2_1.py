d3db8ab3b9b57b56dc31afb2758276037fa3041e
Matthew Krafczyk <krafczyk.matthew@gmail.com>
2023-09-11 11:42:18 -0500

    Improve build_definition method

--- tests/test_core2_1.py
+++ tests/test_core2_1.py
@@ -12,6 +12,7 @@ def test_create_definition_2():
 
 
 def test_create_definition_3():
+    # Plain single level definition
     definition = Definition(objects.TestClass1, 10, test='a')
     assert definition.cls == objects.TestClass1
     assert definition['cls'] == objects.TestClass1
@@ -22,12 +23,29 @@ def test_create_definition_3():
 
 
 def test_build_definition_1():
+    # Plain single level object
     obj = objects.TestClass1(10, test='a')
     definition = build_definition(obj)
     assert definition.cls == objects.TestClass1
     assert definition['cls'] == objects.TestClass1
     assert len(definition.args) == 1
     assert definition.args[0] == 10
-    print(definition.kwargs)
     assert len(definition.kwargs.keys()) == 1
     assert definition.kwargs['test'] == 'a'
+
+
+def test_build_definition_2():
+    # 1 nest object
+    obj = objects.TestClass1(objects.TestClass1(10, test='b'), test='a')
+    definition = build_definition(obj)
+    assert definition.cls == objects.TestClass1
+    assert len(definition.args) == 1
+    assert len(definition.kwargs.keys()) == 1
+    assert definition.kwargs['test'] == 'a'
+    sub_def = definition.args[0]
+    assert type(sub_def) == Definition
+    assert sub_def.cls == objects.TestClass1
+    assert len(sub_def.args) == 1
+    assert sub_def.args[0] == 10
+    assert len(sub_def.kwargs.keys()) == 1
+    assert sub_def.kwargs['test'] == 'b'
