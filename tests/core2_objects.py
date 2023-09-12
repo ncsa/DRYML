2995a5fcaf86b65302d6656bd45489313a33e775
Matthew Krafczyk <krafczyk.matthew@gmail.com>
2023-09-11 23:56:36 -0500

    First implementation of selector

--- tests/core2_objects.py
+++ tests/core2_objects.py
@@ -1,12 +1,20 @@
 from dryml.core2 import Remember, Defer, UniqueID, Metadata
 
+
 class TestClass1(Remember):
     def __init__(self, x, *args, test=None, **kwargs):
         super().__init__(*args, **kwargs)
         self.x = x
         self.test = test
 
+
 class TestClass2(Remember):
     def __init__(self, **kwargs):
         super().__init__()
         self.kwargs = kwargs
+
+
+class TestClass3(Remember):
+    def __init__(self, *args):
+        super().__init__()
+        self.args = args
