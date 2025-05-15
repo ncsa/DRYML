c586afe44b132e19632ed9f4b28d0db97f1a797b
Harsh Vardhan Singhal <reach.harsh.singhal@gmail.com>
2025-05-15 09:32:50 +0530

    Create init.py

---
+++ src/dryml/examples/sentiment_analysis/init.py
@@ -0,0 +1,7 @@
+from dryml.examples.sentiment_analysis.workshop import SentimentWorkshop
+from dryml.examples.sentiment_analysis.torch.models import SentimentTorchModel
+
+__all__ = [
+    "SentimentWorkshop",
+    "SentimentTorchModel"
+]
