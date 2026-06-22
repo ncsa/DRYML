3e9a9098569625469ecf2d21b75629a25d06d0b4
Matthew Krafczyk <krafczyk.matthew@gmail.com>
2026-06-22 14:23:45 -0500

    Forgot a bit from session

--- src/dryml/__init__.py
+++ src/dryml/__init__.py
@@ -2,12 +2,17 @@ import dryml.context as context
 import dryml.core2 as core2
 import dryml.artifacts as artifacts
 import dryml.execute as execute
+from dryml.core2.session import config, configure, reset_config, status
 
 __version__ = "0.3.0-dev"
 
 __all__ = [
-    context,
-    core2,
-    artifacts,
-    execute,
+    "context",
+    "core2",
+    "artifacts",
+    "execute",
+    "config",
+    "configure",
+    "reset_config",
+    "status",
 ]
